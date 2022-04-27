860f580f4bc12e1467a613e2a797ff9af3919a6d
Lucas Morgenster <lucasmorgenster@gmail.com>
2022-04-27 15:26:57 -0300

    Classes_Primitivas

---
+++ src/declaracao_de_classes/tipos_primitivos.java
@@ -0,0 +1,17 @@
+package declaracao_de_classes;
+
+public class tipos_primitivos {
+	public static void main (String args[]) {
+		boolean alternativa=false;
+		char sexo='M';
+		byte idade= 18;
+		short codigo=1024;
+		float nota=9.4f;
+		int alunos=100, classes=10;
+		long habitantes=355376;
+		double dolar=4.95;
+		System.out.println("sexo: " +sexo+ " idade: " +idade+ " código: " +codigo);
+		System.out.println("\nnota: " +nota+ " alunos: " +alunos+ " classes: " +classes);
+		System.out.println("\nhabitantes: " +habitantes+ " dólar: " +dolar+ " alternativa: " +alternativa);
+	}
+}
